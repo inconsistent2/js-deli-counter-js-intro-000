93efd5e9376e1f99ff1fe907e30f9b4b53da93c7
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-14 13:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line,name){
 line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine=""){
+    return "There is nobody waiting to be served!"
+  }else{
+    return katzDeliLine[0];
+    katzDeliLine.shift();
+  }
+}
