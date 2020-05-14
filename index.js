33463efb53863b6ca2bd6fd85a49cfa62c783d09
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-14 14:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line){
 
 function currentLine(line){
   if (line.length===0){
-    return "TThe line is currently empty."
+    return "The line is currently empty."
   }else{
     for (let i=0;i<line.length;i++){
       return `The line is currently; ${i+1}. ${line[i]},`
